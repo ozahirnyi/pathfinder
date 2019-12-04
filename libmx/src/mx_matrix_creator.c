25736c58afcf13a5b1d5f3bbb2015ac3b67bb006
ozahirnyi <ozahirny@gmail.com>
2019-12-04 19:39:49 +0200

    Matrix_creator is done

--- libmx/src/mx_matrix_creator.c
+++ libmx/src/mx_matrix_creator.c
@@ -27,14 +27,16 @@ static int check_duplicates(t_list **list, char *str) {
 static t_list *islands_list(char **islands) {
     t_list *new_list = NULL;
     char *temp = NULL;
+    char **temp_minus = NULL;
+    char **temp_koma = NULL;
 
     for (int i = 1; islands[i] != NULL; i++) {
-        char **temp_minus = mx_strsplit(islands[i], '-');
+        temp_minus = mx_strsplit(islands[i], '-');
             if (!check_duplicates(&new_list, temp_minus[0])) {
                 temp = mx_strdup(temp_minus[0]);
                 mx_push_back(&new_list, temp);
             }
-        char **temp_koma = mx_strsplit(temp_minus[1], ',');
+        temp_koma = mx_strsplit(temp_minus[1], ',');
             if (!check_duplicates(&new_list, temp_koma[0])) {
                 temp = mx_strdup(temp_koma[0]);
                 mx_push_back(&new_list, temp);
@@ -45,33 +47,39 @@ static t_list *islands_list(char **islands) {
     return new_list;
 }
 
-static int **empty_matrix(char **islands) {
-    int size = mx_atoi(islands[0]);
-    int **matrix = (int **)malloc(size * (sizeof(int **)));
-
-    for (int i = 0; i < size; i++) {
-        matrix[i] = (int *)malloc(size * (sizeof(int *)));
-        for (int j = 0; j < size; j++)
-            matrix[i][j] = -1;
-    }
-    return matrix;
-}
-
-int **mx_matrix_creator(char **islands) {
-    int **matrix = empty_matrix(islands);
-    t_list *list = islands_list(islands);
-
+static int **matrix_filling(char **islands, int **matrix, t_list *list) {
+    char **temp_minus;
+    char **temp_koma;
+    int itoe;
+    int jitoe;
+    int value;
+//                                                                      LISHNYAYA PEREMENNAYA!!!11!11!!!!!!!!!!!!
     for (int i = 1; islands[i]; i++) {
-        char **temp_minus = mx_strsplit(islands[i], '-');
-        int itoe = island_index(&list, temp_minus[0]);
-        char **temp_koma = mx_strsplit(temp_minus[1], ',');
-        int jitoe = island_index(&list, temp_koma[0]);
-        int value = mx_atoi(temp_koma[1]);
+        temp_minus = mx_strsplit(islands[i], '-');
+        itoe = island_index(&list, temp_minus[0]);
+        temp_koma = mx_strsplit(temp_minus[1], ',');
+        jitoe = island_index(&list, temp_koma[0]);
+        value = mx_atoi(temp_koma[1]);
+
         matrix[itoe][jitoe] = value;
         matrix[jitoe][itoe] = value;
-
         mx_del_strarr(&temp_minus);
         mx_del_strarr(&temp_koma);
     }
+    return matrix;
+}
+int **mx_matrix_creator(char **islands) {
+    t_list *list = islands_list(islands);
+    int size = mx_atoi(islands[0]);
+    int **matrix = (int **)malloc(size * (sizeof(int **)));
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i < size; i++) {
+        matrix[i] = (int *)malloc(size * (sizeof(int *)));
+        for (j = 0; j < size; j++)
+            matrix[i][j] = -1;
+    }
+    matrix = matrix_filling(islands, matrix, list);
     return matrix;
 }
