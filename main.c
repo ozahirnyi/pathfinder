afb473253eb8d2cb5c8d0ff889e283b8e7f242c6
ozahirnyi <ozahirny@gmail.com>
2020-01-13 18:28:05 +0200

    - 1 declaration in main

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include "pathfinder.h"
 
+void list_filler(t_list *list, mini_list *list1, char *str, int size) {
+    char **islands = mx_strsplit(str, '\n');
+    list = mx_island_list(islands);
+    int **matrix = mx_matrix_creator(islands, list);
+
+    mx_deixtra_cycle(matrix, size, &list1);
+    mx_result_matrix(&list1, size);
+}
+
 int main(int argc, char **argv) {
     char *str = mx_file_to_str("../qwe");
     int size = mx_atoi(str);
-    char **islands = mx_strsplit(str, '\n');
-    t_list *list = mx_island_list(islands);
-    int **matrix = mx_matrix_creator(islands, list);
-    mini_list *list2 = NULL;
+    t_list *list = NULL;
+    mini_list *list1 = NULL;
+
+    list_filler(list, list1, str, size);
 //    while (list) {
 //        printf("%s  |  ", list->data);
 //        list = list->next;
 //    }
-    mx_deixtra_cycle(matrix, size, &list2);
 //    printf("\n");
 //    mini_list *list1 = list2;
 //    while (list1) {
@@ -32,7 +40,6 @@ int main(int argc, char **argv) {
 //        list1 = list1->next;
 //        printf("\n");
 //    }
-    mx_result_matrix(&list2, size);
 
 //    system("leaks pathfinder");
 //    //for (int i = 0; i < mx_atoi(str); i++) {
